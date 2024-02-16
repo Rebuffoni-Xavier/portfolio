92cd5b1278b2f8577ae7e9c10c6ecfb74a0384c5
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2024-02-16 11:52:33 +0100

    début carousel video mobile

--- src/js/home/home.js
+++ src/js/home/home.js
@@ -70,58 +70,117 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Photo Carousel //
 
-let currentIndex = 0;
+let currentIndexPhoto = 0;
 
-function showSlide(index) {
-    const wrapper = document.querySelector('.carouselphoto_items');
-    const itemWidth = document.querySelector('.carouselphoto_items--slide').offsetWidth;
-    currentIndex = index;
+function showSlidePhoto(index) {
+    const wrapperPhoto = document.querySelector('.carouselphoto_items');
+    const itemWidthPhoto = document.querySelector('.carouselphoto_items--slide').offsetWidth;
+    currentIndexPhoto = index;
 
-    wrapper.style.transform = `translateX(${-currentIndex * itemWidth}px)`;
-    updateDots();
-    updateButtonVisibility();
+    wrapperPhoto.style.transform = `translateX(${-currentIndexPhoto * itemWidthPhoto}px)`;
+    updateDotsPhoto();
+    updateButtonVisibilityPhoto();
 }
 
-function nextSlide() {
-    const totalItems = document.querySelectorAll('.carouselphoto_items--slide').length;
-    currentIndex = (currentIndex + 1) % totalItems;
-    showSlide(currentIndex);
+function nextSlidePhoto() {
+    const totalItemsPhoto = document.querySelectorAll('.carouselphoto_items--slide').length;
+    currentIndexPhoto = (currentIndexPhoto + 1) % totalItemsPhoto;
+    showSlidePhoto(currentIndexPhoto);
 }
 
-function prevSlide() {
-    const totalItems = document.querySelectorAll('.carouselphoto_items--slide').length;
-    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-    showSlide(currentIndex);
+function prevSlidePhoto() {
+    const totalItemsPhoto = document.querySelectorAll('.carouselphoto_items--slide').length;
+    currentIndexPhoto = (currentIndexPhoto - 1 + totalItemsPhoto) % totalItemsPhoto;
+    showSlidePhoto(currentIndexPhoto);
 }
 
-function createDots() {
-    const totalItems = document.querySelectorAll('.carouselphoto_items--slide').length;
-    const dotsContainer = document.querySelector('.carouselphoto_dots');
+function createDotsPhoto() {
+    const totalItemsPhoto = document.querySelectorAll('.carouselphoto_items--slide').length;
+    const dotsContainerPhoto = document.querySelector('.carouselphoto_dots');
 
-    for (let i = 0; i < totalItems; i++) {
-      const dot = document.createElement('div');
-      dot.classList.add('photodot');
-      dot.addEventListener('click', () => showSlide(i));
-      dotsContainer.appendChild(dot);
+    for (let i = 0; i < totalItemsPhoto; i++) {
+      const dotPhoto = document.createElement('div');
+      dotPhoto.classList.add('photodot');
+      dotPhoto.addEventListener('click', () => showSlidePhoto(i));
+      dotsContainerPhoto.appendChild(dotPhoto);
     }
 
-    updateDots();
+    updateDotsPhoto();
 }
 
-function updateDots() {
-    const dots = document.querySelectorAll('.photodot');
-    dots.forEach((dot, index) => {
-      dot.classList.toggle('active', index === currentIndex);
+function updateDotsPhoto() {
+    const dotsPhoto = document.querySelectorAll('.photodot');
+    dotsPhoto.forEach((dot, index) => {
+      dot.classList.toggle('active', index === currentIndexPhoto);
     });
 }
 
-function updateButtonVisibility() {
-    const prevBtn = document.querySelector('.carouselphoto_prevbutton');
-    const nextBtn = document.querySelector('.carouselphoto_nextbutton');
-    const totalItems = document.querySelectorAll('.carouselphoto_items--slide').length;
+function updateButtonVisibilityPhoto() {
+    const prevBtnPhoto = document.querySelector('.carouselphoto_prevbutton');
+    const nextBtnPhoto = document.querySelector('.carouselphoto_nextbutton');
+    const totalItemsPhoto = document.querySelectorAll('.carouselphoto_items--slide').length;
 
-    prevBtn.style.display = currentIndex === 0 ? 'none' : 'block';
-    nextBtn.style.display = currentIndex === totalItems - 1 ? 'none' : 'block';
+    prevBtnPhoto.style.display = currentIndexPhoto === 0 ? 'none' : 'block';
+    nextBtnPhoto.style.display = currentIndexPhoto === totalItemsPhoto - 1 ? 'none' : 'block';
 }
-createDots();
-updateButtonVisibility();
+createDotsPhoto();
+updateButtonVisibilityPhoto();
+
+
+// // Video //
+
+let currentIndexVideo = 0;
+
+function showSlideVideo(index) {
+    const wrapperVideo = document.querySelector('.carouselvideo_items');
+    const itemWidthVideo = document.querySelector('.carouselvideo_items--slide').offsetWidth;
+    currentIndexVideo = index;
+
+    wrapperVideo.style.transform = `translateX(${-currentIndexVideo * itemWidthVideo}px)`;
+    updateDotsVideo();
+    updateButtonVisibilityVideo();
+}
+
+function nextSlideVideo() {
+    const totalItemsVideo = document.querySelectorAll('.carouselvideo_items--slide').length;
+    currentIndexVideo = (currentIndexVideo + 1) % totalItemsVideo;
+    showSlideVideo(currentIndexVideo);
+}
+
+function prevSlideVideo() {
+    const totalItemsVideo = document.querySelectorAll('.carouselvideo_items--slide').length;
+    currentIndexVideo = (currentIndexVideo - 1 + totalItemsVideo) % totalItemsVideo;
+    showSlideVideo(currentIndexVideo);
+}
+
+function createDotsVideo() {
+    const totalItemsVideo = document.querySelectorAll('.carouselvideo_items--slide').length;
+    const dotsContainerVideo = document.querySelector('.carouselvideo_dots');
+
+    for (let i = 0; i < totalItemsVideo; i++) {
+      const dotVideo = document.createElement('div');
+      dotVideo.classList.add('videodot');
+      dotVideo.addEventListener('click', () => showSlideVideo(i));
+      dotsContainerVideo.appendChild(dotVideo);
+    }
+
+    updateDotsVideo();
+}
+
+function updateDotsVideo() {
+    const dotsVideo = document.querySelectorAll('.videodot');
+    dotsVideo.forEach((dot, index) => {
+      dot.classList.toggle('active', index === currentIndexVideo);
+    });
+}
+
+function updateButtonVisibilityVideo() {
+    const prevBtnVideo = document.querySelector('.carouselvideo_prevbutton');
+    const nextBtnVideo = document.querySelector('.carouselvideo_nextbutton');
+    const totalItemsVideo = document.querySelectorAll('.carouselvideo_items--slide').length;
+
+    prevBtnVideo.style.display = currentIndexVideo === 0 ? 'none' : 'block';
+    nextBtnVideo.style.display = currentIndexVideo === totalItemsVideo - 1 ? 'none' : 'block';
+}
+createDotsVideo();
+updateButtonVisibilityVideo();
