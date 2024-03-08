443772c71abb86b39cabf4ba146203568e3635d0
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2024-03-08 15:05:05 +0100

    Pages photos v1

--- src/js/photography/flower.js
+++ src/js/photography/flower.js
@@ -1,5 +1,5 @@
 const gallery = document.querySelector('.gallery');
-fetch('../../src/json/photography/flower.json')
+fetch('../../../src/json/photography/flower.json')
     .then(response => response.json())
     .then(data => {
        console.log(data);
@@ -8,6 +8,23 @@ fetch('../../src/json/photography/flower.json')
             img.src = element.src;
             img.alt = element.alt;
             img.classList.add('main_img');
+            img.addEventListener('click', function() { 
+                document.getElementById('carrousel').style.display = 'block';
+                document.body.style.overflow = 'hidden';
+            });
             gallery.appendChild(img);
         });
     });
+
+    
+// Full Screen //
+
+document.getElementById('fullscreen').addEventListener('click', function() {
+    document.getElementById('carrousel').style.display = 'block';
+    document.body.style.overflow = 'hidden';
+});
+  
+document.getElementById('close').addEventListener('click', function() {
+    document.getElementById('carrousel').style.display = 'none';
+    document.body.style.overflow = 'auto';
+});
